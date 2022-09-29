29f0cb7e555b7a7227fa10e392bd628b8953b7e3
PC-TICS12 <a3519110011@alumno.uttehuacan.edu.mx>
2022-09-29 17:53:07 -0500

    primer commit

---
+++ src/components/Formulario.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+    data: () => ({
+       proyecto:"",
+       tipo:"",
+       urgente:false,
+      proyectos:[],
+
+    
+    }),
+    methods: {
+        registrarProyecto(){
+            const proyecto={
+                proyecto: this.proyecto,
+                tipo: this.tipo,
+                urgente: this.urgente,
+
+            };
+            this.proyectos.push(proyecto);
+
+            this.proyecto="";
+            this.tipo="";
+            this.urgente=false;
+            console.log(this.proyectos)    ;
+        },
+    }
+};
+</script>
+
+<template>
+    <form @submit.prevent="registrarProyecto">
+        <div class="mb-3">
+            <label class="form-label"> Actividad </label>
+            <input class="form-control" v-model.trim="proyecto" type="text" placeholder="Actividad" required />
+      
+            <!--
+      <input class="form-control" v-model="nombre" type="text" placeholder="Nombre" required />
+      
+            <label class="form-label">Edad: </label>
+            <input class="form-control" v-model="edad" type="number" min="15" max="80" placeholder="Edad" required />
+      
+            -->
+       <select required v-model="tipo">
+        <option disabled selected value=""  > Selecciona un tipo de actividad</option>
+        <option value="1">Aplicaciones web con Vue.js</option>
+        <option value="2">Backend Services con Node.js</option>
+        <option value="3">App movil con react Native</option>
+        
+       </select>
+        </div>
+        <div class="mb-3">
+
+            <label> urgente</label>
+     <input type="checkbox" v-model="urgente" class="form-check-input" >
+
+        </div>
+        <button class="btn btn-success btn-block" type="submit">guardar</button>
+    </form>
+
+
+
+    <div class="table-responsive">
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th> #
+            </th>
+            <th>  proyecto
+            </th>
+            <th>  tipo
+            </th>
+            <th>  urgente
+            </th>
+                </tr>
+               
+            </thead>
+            <tbody>
+                <td>{{proyecto}}</td>
+                <td></td>
+                <td></td>
+                <td class="bg-danger"></td>
+            </tbody>
+           
+        </table>
+    </div>
+</template>
