40f264cd7fc3ea85643dd04e674c2ddbfff46811
MiltonRubio <a3519110011@alumno.uttehuacan.edu.mx>
2022-09-30 15:31:38 -0500

    segundo commit

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -5,30 +5,48 @@ export default {
        tipo:"",
        urgente:false,
       proyectos:[],
+      numeroProyectos:0,
 
     
     }),
     methods: {
+
+        cambiarUrgencia(proyecto, campo){
+           // this.proyectos[id].urgente= !this.proyectos[id].urgente;
+          console.log(proyecto, campo);
+          proyecto[campo]=!proyecto[campo];
+
+        },
         registrarProyecto(){
             const proyecto={
                 proyecto: this.proyecto,
                 tipo: this.tipo,
                 urgente: this.urgente,
+                completado:false,
 
             };
             this.proyectos.push(proyecto);
+         
 
             this.proyecto="";
             this.tipo="";
             this.urgente=false;
             console.log(this.proyectos)    ;
         },
-    }
+     
+    }  , 
+    computed:{
+            numeroProyectos(){
+                return this.proyectos.length;
+            }
+        }
 };
 </script>
 
 <template>
-    <form @submit.prevent="registrarProyecto">
+   
+    <div class="row">
+        <div class="col-12 col-md-4">  <form @submit.prevent="registrarProyecto">
         <div class="mb-3">
             <label class="form-label"> Actividad </label>
             <input class="form-control" v-model.trim="proyecto" type="text" placeholder="Actividad" required />
@@ -55,12 +73,18 @@ export default {
 
         </div>
         <button class="btn btn-success btn-block" type="submit">guardar</button>
-    </form>
+    </form>  </div>
+
+
+            <div class="col-12 col-md-8">
 
 
 
-    <div class="table-responsive">
-        <table class="table table-dark">
+<h3> Total de proyectos: {{numeroProyectos}}</h3>   
+
+   
+                <div class="table-responsive">
+        <table class="table table-dark ">
             <thead>
                 <tr>
                     <th> #
@@ -71,16 +95,27 @@ export default {
             </th>
             <th>  urgente
             </th>
+            <th> Completado
+            </th>
                 </tr>
                
             </thead>
             <tbody>
-                <td>{{proyecto}}</td>
-                <td></td>
-                <td></td>
-                <td class="bg-danger"></td>
+                <tr v-for="(proyecto, index) in proyectos" :key="index" >
+                    <td>{{index +1}}</td>
+                    <td>{{proyecto.proyecto}}</td>
+                    <td>{{proyecto.tipo}}</td>
+                    <td @click="cambiarUrgencia(proyecto, 'urgente')" :class="proyecto.urgente ?  'bg-danger':'bg-success' " > {{proyecto.urgente ? "si" : "no" }}</td>
+                    <td @click="cambiarUrgencia(proyecto, 'completado')" :class="proyecto.completado ?  'bg-success':'bg-danger' " > {{proyecto.completado ? "completado" : "incompleto" }}</td>
+                 
+                </tr>
+                
+              
             </tbody>
            
         </table>
+    </div>
+            </div>
+     
     </div>
 </template>
